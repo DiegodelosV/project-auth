74b23d6dd96a26e49dbf37db1944330900a95125
DiegodelosV <diegodelosv@gmail.com>
2024-09-26 16:30:40 -0300

    Se usa beforeEach para hacer accesibles las vistas Home y About solo para usuarios registrados.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <nav>
-      <router-link to="/">Home</router-link>
+      <router-link v-if="isLoggedIn" to="/">Home</router-link>
       <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
       <router-link v-if="!isLoggedIn" to="/registro">Registro</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link v-if="isLoggedIn" to="/about">About</router-link>
       <button class="btnLogout" v-if="isLoggedIn" @click="logout">Cerrar sesion</button>
     </nav>
     <main>
